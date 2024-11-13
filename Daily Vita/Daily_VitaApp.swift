289066c91230164fd37d4725f8a28396368c23f6
KhinPhone060 <60284009+KhinPhone060@users.noreply.github.com>
2024-11-13 08:32:20 +0630

    Initial Commit

---
+++ Daily Vita/Daily_VitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_VitaApp.swift
+//  Daily Vita
+//
+//  Created by Khin Phone Ei on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Daily_VitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
